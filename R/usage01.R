2a996edbcb34f647998c98c190eb41fe7d2c69d2
Killua-github <y.h.08491911@gmail.com>
2020-10-02 12:41:52 +0900

    usage01

---
+++ R/usage01.R
@@ -0,0 +1,24 @@
+usage01 <- function()
+{
+  print("usageはRの使い方を説明する関数です．")
+  print("上野研でRはDAM解析に使います．")
+  print("関数は「usage」のようなRに打つ指示と認識していれば問題ないです．")
+  print("「library(flysleep)」→「x<-DAMsleep1()」→「DAMsleep2(x)」")
+  print("このように順番に指示することで簡単にDAMの解析ができます．")
+  print("指示する関数は「」ごとに>の後に1行ずつ打ちます")
+  print("解析がすべて終わるとその結果が「sleep2graph.png」
+        という名前で保存されます．")
+  print("「DAMsleep2」以外の解析方法は
+        「https://github.com/ueno-tr/flysleep」で確認できます．")
+  print("以下は関数を打った時に出る問題について説明しています．")
+  print("usage02：「usage01()」と指示するとErrorが出る")
+  print("")
+  print("")
+  print("")
+  print("")
+  print("")
+  print("")
+  print("")
+  print("")
+  
+}
