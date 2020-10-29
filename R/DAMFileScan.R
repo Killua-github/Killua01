89af50c9c1159e06afa3b05db197f5f07d31690f
Killua-github <y.h.08491911@gmail.com>
2020-10-30 02:04:37 +0900

    DAMFileScan

--- R/DAMFileScan.R
+++ R/DAMFileScan.R
@@ -1,12 +1,21 @@
-DAMFileScan <- function(readname, number, writenmae)
+D <- function()
 {
-  #data.tableを呼び出す
+  library("lubridate")
+  date1 <- ("16 Sep 20")
+  date2 <- ("16 Sep 20")
+  startdate <- dmy_hms(date1)
+  enddate <- dmy_hms(date2)
+  cut_interval <- interval(start = startdate, end = enddate)
+  filelists <- list.files(pattern = "Monitor...txt$")
   library("data.table")
-  #元データを読み込む（ヘッターなし）
-  file <- fread(readname)
-  
-  #行の選択
-  cutfile <- file[number:(number+4319), ]
-  write.table(cutfile, "writename",
-              sep = "\t", row.names = F, col.names = F)
+  for (i in 1:length(filelists))
+  {
+    file <- fread(filelists[i])
+    #データを日付型にする
+    timecolumn <- as.POSIXlt(file$V2:V3, format = "%d%m%Y %H:%M:%S",
+                             tz = "Japan")
+    cut_file <- file[cut_interval, ]
+    write.table(cut_file, file = "2010091919CtM019.txt",
+                sep = "\t", row.names = F, col.names = F, quote = F)
+  }
 }
