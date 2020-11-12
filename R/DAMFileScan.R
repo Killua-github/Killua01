66fdc6a40324bf87e0124c14ded6cd4d959534d7
Killua-github <y.h.08491911@gmail.com>
2020-11-12 21:01:58 +0900

    DAMFileScan

---
+++ R/DAMFileScan.R
@@ -0,0 +1,20 @@
+DAMFileScan <- function(Monitor, dd, mm, yy)
+{
+  DAMfileList <- list.files(pattern = "Monitor...txt")
+  library(data.table)
+  for (i in 1:length(DAMfileList))
+  {
+    DAMdata <- DAMfileList[i]
+    DAMfile <- fread(DAMdata)
+    c1 <- DAMfile[V2==paste(dd, mm, yy, sep = " "),]
+    c2 <- DAMfile[V2==paste(dd +1, mm, yy, sep = " "),]
+    c3 <- DAMfile[V2==paste(dd +2, mm, yy, sep = " "),]
+    c4 <- DAMfile[V2==paste(dd +3, mm, yy, sep = " "),]
+    cutDAMfile <- rbindlist(list(c1, c2, c3, c4))
+    CutDAMFile <- cutDAMfile[541:4860, ]
+    write.table(CutDAMFile, paste(dd, mm, yy, "CtM", 0,
+                                  Monitor - 1 + i, ".txt", sep = ""),
+                sep = "\t", row.names = FALSE, col.names = FALSE,
+                quote = FALSE)
+  }
+}
