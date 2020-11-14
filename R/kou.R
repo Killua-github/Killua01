a52d9fec26aa8433904e7832ecfe3bed8559f59a
Killua-github <y.h.08491911@gmail.com>
2020-11-14 14:52:43 +0900

    kou DAMFileScan kDAMsleep1

---
+++ R/kou.R
@@ -0,0 +1,101 @@
+kou <- function(light, colour, temp1, temp2, temp3)
+{
+  library(readxl)
+  data <- read_excel("yuna.xls")
+  if(light == "LD")
+  {
+    if(colour == "B")
+    {
+      png(file = paste(light, "_", colour, "_",
+                       temp1, ".png", sep = ""),
+          width = 1000, height = 400)
+      COWorRAT(light)
+      par(new = T)
+      arrow(colour, temp1, temp2, temp3)
+      dev.off()
+    }
+    else if(colour == "R")
+    {
+      png(file = paste(light, "_", colour, "_",
+                       temp1, ".png", sep = ""),
+          width = 1000, height = 400)
+      COWorRAT(light)
+      par(new = T)
+      arrow(colour, temp1, temp2, temp3)
+      dev.off()
+    }
+    else if(colour == "BRB")
+    {
+      png(file = paste(light, "_", colour, "_",
+                       temp1, temp2, temp3, ".png", sep = ""),
+          width = 1000, height = 400)
+      COWorRAT(light)
+      par(new = T)
+      arrow(colour, temp1, temp2, temp3)
+      dev.off()
+    }
+    else if(colour == "RBR")
+    {
+      png(file = paste(light, "_", colour, "_",
+                       temp1, temp2, temp3, ".png", sep = ""),
+          width = 1000, height = 400)
+      COWorRAT(light)
+      par(new = T)
+      arrow(colour, temp1, temp2, temp3)
+      dev.off()
+    }
+    else
+    {
+      noquote("error colour argument")
+    }
+  }
+  else if(light == "DD")
+  {
+    if(colour == "B")
+    {
+      png(file = paste(light, colour, temp1, ".png", sep = ""),
+          width = 1000, height = 400)
+      COWorRAT(light)
+      par(new = T)
+      arrow(colour, temp1, temp2, temp3)
+      dev.off()
+    }
+    else if(colour == "R")
+    {
+      png(file = paste(light, colour, temp1, ".png", sep = ""),
+          width = 1000, height = 400)
+      COWorRAT(light)
+      par(new = T)
+      arrow(colour, temp1, temp2, temp3)
+      dev.off()
+    }
+    else if(colour == "BRB")
+    {
+      png(file = paste(light, colour, temp1, temp2, temp3,
+                       ".png", sep = ""),
+          width = 1000, height = 400)
+      COWorRAT(light)
+      par(new = T)
+      arrow(colour, temp1, temp2, temp3)
+      dev.off()
+    }
+    else if(colour == "RBR")
+    {
+      png(file = paste(light, colour, temp1, temp2, temp3,
+                       ".png", sep = ""),
+          width = 1000, height = 400)
+      COWorRAT(light)
+      par(new = T)
+      arrow(colour, temp1, temp2, temp3)
+      dev.off()
+    }
+    else
+    {
+      noquote("error colour argument")
+    }
+  }
+  else
+  {
+    noquote("error light argument")
+  }
+}
