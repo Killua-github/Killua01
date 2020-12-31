40da54686a3a2e43218ac0a83141d8a006d5c9b7
Killua-github <y.h.08491911@gmail.com>
2020-12-31 17:31:47 +0900

    AMimportMarray

--- R/AMimportMarray.R
+++ R/AMimportMarray.R
@@ -1,19 +1,19 @@
 AMimportMarray <- function()
 {
   files <- list.files(pattern = "......M0..AM.txt")
-  t <- array(0, dim = c(720 * 3, 32, length(files)))
+  AMMarray <- array(0, dim = c(720 * 3, 32, length(files)))
   for (i in 1:length(files))
   {
     file.name <- files[i]
     a <- data.matrix(read.table(file.name, sep = "\t"))
     b <- a[, 11:42]
     colnames(b) <- NULL
-    t[, , i] <- b
+    AMMarray[, , i] <- b
   }
   for (j in 1:length(files))
   {
     files[j] <- substring(files[j], nchar(files[j]) -9, nchar(files[j]) -4)
   }
-  dimnames(t) <- list(rownames(t), colnames(t), files)
+  dimnames(AMMarray) <- list(rownames(AMMarray), colnames(AMMarray), files)
   return(AMMarray)
 }
