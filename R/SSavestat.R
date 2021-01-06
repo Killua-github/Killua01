c801432195d9ef370ac39531b5b92fcc92c5b77c
Killua-github <y.h.08491911@gmail.com>
2021-01-06 14:40:07 +0900

    SSavestat

--- R/SSavestat.R
+++ R/SSavestat.R
@@ -3,11 +3,11 @@ SSavestat <- function(LDstat, Summary, LD)
   Channel <- matrix(rep(paste0("C", c(1:32))), ncol = 32)
   row.names(Channel) <- Summary[1, 1]
   LDStat <- LDstat[, , 1]
-  LDStat <- rbind(b, LDStat)
+  LDStat <- rbind(Channel, LDStat)
   for (i in 2:length(Summary[, 1]))
   {
-    row.names(b) <- Summary[i, 1]
-    LDStat <- rbind(LDStat, b, LDstat[, , i])
+    row.names(Channel) <- Summary[i, 1]
+    LDStat <- rbind(LDStat, Channel, LDstat[, , i])
   }
   write.table(LDStat, paste0("stat", "_", LD, ".txt"),
               col.names = FALSE, row.names = TRUE, sep = "\t", quote = FALSE)
