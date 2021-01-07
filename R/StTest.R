5b1186c764350bf64a0a91894f6056d52e25ab53
Killua-github <y.h.08491911@gmail.com>
2021-01-07 16:57:54 +0900

    StTest

--- R/StTest.R
+++ R/StTest.R
@@ -9,26 +9,26 @@ StTest <- function(LD)
   LDstat <- SDAMstat(LDMarray, LDsleep, LD)
   SSavestat(LDstat, Summary, LD)
   Length1 <- Summary[1, 2]:Summary[1, 3]
-  tstat1 <- LDstat[, Length, 1]
+  tstat1 <- LDstat[, Length1, 1]
   if (is.na(Summary[1, 5]))
   {
     Tstat1 <- tstat1
   }
   else
   {
-    exclude <- as.integer(unlist(strsplit(Summary[1,5], ",")))
+    exclude <- as.integer(unlist(strsplit(Summary[1, 5], ",")))
     Exclude <- exclude - as.numeric(Summary[1, 2]) + 1
     Tstat1 <- tstat1[, -Exclude]
   }
   Length2 <- Summary[2, 2]:Summary[2, 3]
-  tstat2 <- LDstat[, Length, 2]
+  tstat2 <- LDstat[, Length2, 2]
   if (is.na(Summary[2, 5]) == TRUE)
   {
     Tstat2 <- tstat2
   }
   else
   {
-    exclude <- as.integer(unlist(strsplit(Summary[2,5], ",")))
+    exclude <- as.integer(unlist(strsplit(Summary[2, 5], ",")))
     Exclude <- exclude - as.numeric(Summary[2, 2]) + 1
     Tstat2 <- tstat2[, -Exclude]
   }
