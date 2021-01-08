640bedadcc5c6a52193d9975880cd0eb24be9c01
Killua-github <y.h.08491911@gmail.com>
2021-01-08 18:16:38 +0900

    kDAMsleep2

--- R/kDAMsleep2.R
+++ R/kDAMsleep2.R
@@ -4,10 +4,12 @@ kDAMsleep2 <- function (hrs)
   summary <- read_excel("summary.xls")
   out <- meanSEM(hrs, summary)
   library(ggplot2)
-  p <- ggplot(out, aes_string(x = "time", y = "mean", 
-                              group = names(out)[2], colour = names(out)[2])) + geom_line() + 
-    ylab("sleep (min/hr)")
+  p <- ggplot(out, aes_string(x = "time", y = "mean", group = names(out)[2],
+                              colour = names(out)[2])) + 
+              geom_line() + ylab("sleep (min/hr)") + ylim(0, 60)
   errors <- aes(ymax = mean + SEM, ymin = mean - SEM)
   p <- p + geom_errorbar(errors, width = 0.2) + geom_point(size = 2)
   p
+  SAVEpng("sleep2graph.png")
+  SAVEpdf("sleep2graph.png")
 }
