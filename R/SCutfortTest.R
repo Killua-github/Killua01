da11a1fd85d81f9a512210170f992af69a0457f0
Killua-github <y.h.08491911@gmail.com>
2021-02-07 01:20:28 +0900

    SCutfortTest

--- R/SCutfortTest.R
+++ R/SCutfortTest.R
@@ -4,20 +4,20 @@ SCutfortTest <- function(LD, Outline)
   {
     CtDAMfile <- list.files(pattern = paste0("Ct", Outline[i, 1], ".txt"))
     CtDAMdata <- fread(CtDAMfile)
-    d1 <- CtDAMdata[(1440*0+1):(1440*0+1440),]
-    d2 <- CtDAMdata[(1440*1+1):(1440*1+1440),]
-    d3 <- CtDAMdata[(1440*2+1):(1440*2+1440),]
+    d1 <- CtDAMdata[(1440 * 0 + 1):(1440 * 0 + 1440), ]
+    d2 <- CtDAMdata[(1440 * 1 + 1):(1440 * 1 + 1440), ]
+    d3 <- CtDAMdata[(1440 * 2 + 1):(1440 * 2 + 1440), ]
     if (LD == "L")
     {
-      LDd1 <- d1[1:720,]
-      LDd2 <- d2[1:720,]
-      LDd3 <- d3[1:720,]
+      LDd1 <- d1[1:720, ]
+      LDd2 <- d2[1:720, ]
+      LDd3 <- d3[1:720, ]
     }
     else if (LD == "D")
     {
-      LDd1 <- d1[721:1440,]
-      LDd2 <- d2[721:1440,]
-      LDd3 <- d3[721:1440,]
+      LDd1 <- d1[721:1440, ]
+      LDd2 <- d2[721:1440, ]
+      LDd3 <- d3[721:1440, ]
     }
     LDdata <- rbindlist(list(LDd1, LDd2, LDd3))
     date <- substr(CtDAMfile, start = 1, stop = 6)
